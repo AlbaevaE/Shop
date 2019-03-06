8da19797111f805500e2ed01ebece5407214f4f8
elvira <aelvira312@gmail.com>
2019-03-06 18:34:01 +0600

    commit2

--- src/main/Product/It/Project/Dao/ProductDao.java
+++ src/main/Product/It/Project/Dao/ProductDao.java
@@ -1,4 +1,38 @@
 package It.Project.Dao;
 
+import java.sql.*;
+
 public class ProductDao {
+
+    private final String url = "jdbc:postgresql://localhost:5432/";
+    private final String user = "postgres";
+    private final String password = "Babushka85";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+
+    }
+
+    public int getProductCount() {
+        String SQL = "SELECT count(*) FROM product";
+        int count = 0;
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(SQL)) {
+            rs.next();
+            count = rs.getInt(1);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return count;
+    }
 }
+
+
