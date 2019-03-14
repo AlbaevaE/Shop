b1ab4615d284fd5591850373286af394e29e08ca
elvira <aelvira312@gmail.com>
2019-03-14 20:11:04 +0600

    commit7

--- src/main/Product/It/Project/Dao/SizeDao.java
+++ src/main/Product/It/Project/Dao/SizeDao.java
@@ -1,5 +1,41 @@
 package It.Project.Dao;
 
-public class SizeDao {
+import java.sql.*;
 
+public class SizeDao {
+    private final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private final String url1 = "jdbc:postgresql:http://138.68.52.248:5432/gr11";
+    private final String user = "gruppa11";
+    private final String password = "1e23qwe34";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public int getSize(int id) {
+        String SQL = "Select * from sizes";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            ResultSet rs = stmt.executeQuery(SQL);{
+                rs.next();
+            }
+
+        }
+        catch (SQLException ex){
+                 ex.getMessage();
+        }
+        return id;
+    }
 }
