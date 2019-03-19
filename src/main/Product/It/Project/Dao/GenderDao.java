1715dd58563f8746dd6d539db561557732f67208
AseliaA <aselia.azimkanova@gmail.com>
2019-03-19 18:51:20 +0600

    Made a dao and service for gender

--- src/main/Product/It/Project/Dao/GenderDao.java
+++ src/main/Product/It/Project/Dao/GenderDao.java
@@ -4,6 +4,8 @@ package It.Project.Dao;
 import It.Project.Model.Gender;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GenderDao {
     private static final String url = "jdbc:postgresql://138.68.52.248:5432/gr11";
@@ -26,6 +28,25 @@ public class GenderDao {
         return conn;
     }
 
+    public List<Gender> getAllGender(){
+        String SQL = "Select * from gender;";
+        List<Gender> genders = new ArrayList<>();
+
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                genders.add(new Gender(rs.getInt("id") , rs.getString("gender")));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+        }
+        return genders;
+    }
+
     public Gender getGender(int id) {
         String SQL = "Select id, gender from gender where id = ?;";
         Gender gender = new Gender();
@@ -46,5 +67,58 @@ public class GenderDao {
             System.out.println("Error");
         }
         return gender;
+    }
+
+
+    public Gender addGender(Gender gender) {
+        String SQL = "Insert into gender(gender) values(?);";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.setString(1, gender.getGender());
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+        }
+        return gender;
+    }
+
+
+    public Gender updateGender(Gender gender) {
+        String SQL = "Update gender set gender=? where id = ?;";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.setString(1, gender.getGender());
+            stmt.setInt(2, gender.getId());
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+        }
+        return gender;
+    }
+
+
+    public boolean deleteGender(int id){
+        String SQL = "Delete from gender where id=?;";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.setInt(1, id);
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+            return false;
+        }
+        return true;
     }
 }
--- src/main/Product/It/Project/Service/GenderService.java
+++ src/main/Product/It/Project/Service/GenderService.java
@@ -4,11 +4,9 @@ package It.Project.Service;
 import It.Project.Dao.GenderDao;
 import It.Project.Model.Gender;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/gender")
 public class GenderService {
@@ -21,4 +19,43 @@ public class GenderService {
         Gender gender = genderDao.getGender(genderId);
         return gender;
     }
+
+    @POST
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Gender addGender_JSON(Gender gender){
+        System.out.println("Adding a gender");
+        GenderDao genderDao = new GenderDao();
+        genderDao.addGender(gender);
+        return gender;
+    }
+
+    @PUT
+    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    public Gender updateGender_JSON(Gender gender){
+        System.out.println("Updating gender");
+        GenderDao genderDao = new GenderDao();
+        genderDao.updateGender(gender);
+        return gender;
+    }
+
+    @DELETE
+    @Path("{deleteId}")
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public boolean deleteGender(@PathParam("deleteId") Integer id){
+        System.out.println("Deleting gender");
+        GenderDao genderDao = new GenderDao();
+        genderDao.deleteGender(id);
+        return true;
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    public List<Gender> getAllGenders(){
+        System.out.println("Getting all genders");
+        GenderDao genderDao = new GenderDao();
+        List<Gender> genders = genderDao.getAllGender();
+        return genders;
+    }
+
+
 }
