8f23e51cfee9da9f02699b916c14b2fefb7c2c1b
AseliaA <aselia.azimkanova@gmail.com>
2019-03-20 17:19:13 +0600

    Making a dao and service for type

--- src/main/Product/It/Project/Dao/TypeDao.java
+++ src/main/Product/It/Project/Dao/TypeDao.java
@@ -6,7 +6,6 @@ import It.Project.Model.Type;
 
 
 public class TypeDao {
-    // private final String url = "jdbc:postgresql://localhost:5432/postgres";
     private static final String url = "jdbc:postgresql://138.68.52.248:5432/gr11";
     private static final String user = "gruppa11";
     private static final String password = "1e23qwe34";
@@ -28,7 +27,7 @@ public class TypeDao {
     }
 
     public Type getType(int id) {
-        String SQL = "Select id , name , type_product_id from type_product where id = ?;";
+        String SQL = "Select id , name , parent_type_id from type_product where id = ?;";
 
         Type type = new Type();
         try (Connection conn = connect();
@@ -48,5 +47,57 @@ public class TypeDao {
             System.out.println("Error");
         }
         return type;
+    }
+
+    public Type addType(Type type){
+        String SQL = "Insert into type_product(name) values(?);";
+
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.setString(1, type.getName());
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+        }
+        return type;
+    }
+
+    public Type updateType(Type type){
+        String SQL = "Update type_product set name=? where id = ?;";
+
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.setString(1,type.getName());
+            stmt.setInt(2, type.getId());
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+        }
+        return type;
+    }
+
+    public boolean deleteType(int id){
+        String SQL = "Delete from type_product where id=?;";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.setInt(1, id);
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+            return false;
+        }
+        return true;
     }
 }
--- src/main/Product/It/Project/Service/TypeService.java
+++ src/main/Product/It/Project/Service/TypeService.java
@@ -5,21 +5,43 @@ import It.Project.Dao.TypeDao;
 import It.Project.Model.Type;
 
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/type")
 public class TypeService {
     @GET
     @Path("/{typeId}")
-    @Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
     public Type getType_JSON(@PathParam("typeId") Integer typeId){
         System.out.println("Getting Type");
         TypeDao typeDao = new TypeDao();
         Type type = typeDao.getType(typeId);
         return type;
+    }
+
+    @POST
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Type addType_JSON(Type type){
+        System.out.println("Adding type");
+        TypeDao typeDao = new TypeDao();
+        return typeDao.addType(type);
+    }
+
+    @PUT
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Type updateType_JSON(Type type){
+        System.out.printf("Updating type");
+        TypeDao typeDao = new TypeDao();
+        return typeDao.updateType(type);
+    }
+
+    @DELETE
+    @Path("{deleteId}")
+    @Produces
+    public boolean deleteType(@PathParam("deleteID") Integer deleteId){
+        System.out.printf("Deleting type");
+        TypeDao typeDao = new TypeDao();
+        return typeDao.deleteType(deleteId);
     }
 }
