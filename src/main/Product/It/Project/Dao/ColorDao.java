42f1161e93f429ed4dd8391a00aaca759ebd5405
AseliaA <aselia.azimkanova@gmail.com>
2019-03-23 16:08:30 +0600

    Made a dao and service for color

--- src/main/Product/It/Project/Dao/ColorDao.java
+++ src/main/Product/It/Project/Dao/ColorDao.java
@@ -2,7 +2,10 @@ package It.Project.Dao;
 
 import It.Project.Model.Color;
 
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ColorDao {
 
@@ -46,6 +49,77 @@ public class ColorDao {
             System.out.println("Error");
         }
         return color;
+    }
+
+
+    public List<Color> getAllColor() {
+        String SQL = "Select * from color;";
+        List<Color> colors = new ArrayList<>();
+
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                colors.add(new Color(rs.getInt("id"), rs.getString("color")));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+        }
+        return colors;
+    }
+
+    public Color addColor(Color color) {
+        String SQL = "Insert into color(color) values(?);";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.setString(1, color.getColor());
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+        }
+        return color;
+    }
+
+    public Color updateColor(Color color) {
+        String SQL = "Update color set color=? where id = ?;";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.setString(1, color.getColor());
+            stmt.setInt(2, color.getId());
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+        }
+        return color;
+    }
+
+
+    public boolean deleteColor(int id) {
+        String SQL = "Delete from color where id=?;";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(SQL)
+        ) {
+            stmt.setInt(1, id);
+            stmt.execute();
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Error");
+            return false;
+        }
+        return true;
     }
 }
 
--- src/main/Product/It/Project/Service/ColorService.java
+++ src/main/Product/It/Project/Service/ColorService.java
@@ -1,23 +1,62 @@
 package It.Project.Service;
 
 import It.Project.Dao.ColorDao;
+
 import It.Project.Model.Color;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
+import java.util.List;
 
 @Path("/color")
 public class ColorService {
     @GET
     @Path("{colorId}")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Color getColor_JSON(@PathParam("colorId") Integer colorId){
+    public Color getColor_JSON(@PathParam("colorId") Integer colorId) {
         System.out.println("Getting color");
         ColorDao colorDao = new ColorDao();
         Color color = colorDao.getColor(colorId);
         return color;
+    }
+
+    @POST
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Color addColor_JSON(Color color) {
+        System.out.println("Adding color");
+        ColorDao colorDao = new ColorDao();
+        colorDao.addColor(color);
+        return color;
+    }
+
+
+    @PUT
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Color updateColor_JSON(Color color) {
+        System.out.println("Updating color");
+        ColorDao colorDao = new ColorDao();
+        colorDao.updateColor(color);
+        return color;
+    }
+
+    @DELETE
+    @Path("{deleteId}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public boolean deleteGender(@PathParam("deleteId") Integer id) {
+        System.out.println("Deleting color");
+        ColorDao colorDao = new ColorDao();
+        colorDao.deleteColor(id);
+        return true;
+    }
+
+
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Color> getAllColors() {
+        System.out.println("Getting all colors");
+        ColorDao colorDao = new ColorDao();
+        List<Color> colors = colorDao.getAllColor();
+        return colors;
     }
 }
