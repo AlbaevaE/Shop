b9a91140e2721fbcd4c65a37db98fd2fec6d621e
lnkk <aigerimsamidinovaa@gmail.com>
2019-03-24 14:01:59 +0600

    shop Aigerim

--- src/main/Product/It/Project/Model/Shop.java
+++ src/main/Product/It/Project/Model/Shop.java
@@ -3,24 +3,31 @@ package It.Project.Model;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.awt.*;
+
 @XmlRootElement(name="shop")
 @XmlAccessorType(XmlAccessType.FIELD)
-
 public class Shop {
     int id;
     String name;
-    Contact contact;
+    String password;
+    String number;
+    String adress;
+    String email;
 
-    public Shop() {
-    }
-
-    public Shop(int id, String name, Contact contact) {
+    public Shop(int id, String name, String password, String number, String adress, String email) {
         this.id = id;
         this.name = name;
-        this.contact = contact;
+        this.password = password;
+        this.number = number;
+        this.adress = adress;
+        this.email = email;
     }
 
+    public Shop(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
     public int getId() {
         return id;
     }
@@ -37,11 +44,35 @@ public class Shop {
         this.name = name;
     }
 
-    public Contact getContact() {
-        return contact;
+    public String getPassword() {
+        return password;
     }
 
-    public void setContact(Contact contact) {
-        this.contact = contact;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
